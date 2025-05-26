6fb809a6f0045a7b02aa4a8b390062fadb5eaf8b
omelkis <pav.hry@gmail.com>
2025-05-26 18:27:38 +0200

    Create orders.tsx

---
+++ pages/orders.tsx
@@ -0,0 +1,8 @@
+export default function Orders() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Seznam zakázek</h1>
+      <p>Tady budou zobrazeny zakázky.</p>
+    </main>
+  )
+}
